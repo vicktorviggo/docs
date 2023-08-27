1b28e883d0675a61799e91493eb229118e148cbe
Mycroft <kaseyfitton@hotmail.co.uk>
2023-08-27 03:01:34 +0100

    tweak(Config): Project Naming

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Mycroft Studios</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/mycroft-studios',
   },
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Dedicated To Excellence.',
   },
 }
 
