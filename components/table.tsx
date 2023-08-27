a33bf1bdebc0176f3370de1b7e55f34b9c7f13d2
Mycroft <kaseyfitton@hotmail.co.uk>
2023-08-27 16:31:05 -0700

    tables and cards

--- components/table.tsx
+++ components/table.tsx
@@ -6,6 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import styles from './table.module.css'
 
 interface DialogTabProps {
     head: any,
@@ -18,12 +19,17 @@ const DataTable: React.FC<DialogTabProps> = ({
     rows
 }) => {
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} classes={styles} sx={{borderRadius: "5px 5px 0px 0px"}}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             {head.map((row) => (
-                <TableCell align="right">{row}</TableCell>
+                <TableCell 
+                variant="head" 
+                align={row.align}
+                size= "small"
+                sx={{backgroundColor: "#0d0d0d", color: "#fafbfc",}}
+                >{row.name}</TableCell>
             ))}
           </TableRow>
         </TableHead>
@@ -31,12 +37,12 @@ const DataTable: React.FC<DialogTabProps> = ({
           {rows.map((row) => (
             <TableRow
               key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 }, backgroundColor: "#1f1f1f", color: "#cacccf"}}
             >
-              <TableCell>
+              <TableCell sx={{color: "#fafbfc"}}>
                 {row.Number}
               </TableCell>
-              <TableCell align="right">{row.DLC}</TableCell>
+              <TableCell variant="body" size= "small"align="right" sx={{ color: "#fafbfc"} }>{row.DLC}</TableCell>
             </TableRow>
           ))}
         </TableBody>
