3a2499153c892d7ccba2e4f462a2c2baa6e16e35
Mycroft <kaseyfitton@hotmail.co.uk>
2023-09-21 18:29:58 +0100

    feat: definiton text

--- components/link_button.tsx
+++ components/link_button.tsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 import Modal from '@mui/material/Modal';
 import {blue} from "@mui/material/colors";
 
