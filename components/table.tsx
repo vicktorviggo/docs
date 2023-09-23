cf909e94be494b8915a158d4c2a5cbe7e51ed0a4
Mycroft <kaseyfitton@hotmail.co.uk>
2023-09-23 16:02:02 +0100

    some fixes

--- components/table.tsx
+++ components/table.tsx
@@ -16,38 +16,40 @@ const Table: React.FC<DialogTabProps> = ({
     rows
 }) => {
     return (
-        <MantineProvider theme={{
-                fontSizes: {
-                    sm: rem(16),
+        <div style={{ zIndex: 10000 }}>
+            <MantineProvider theme={{
+                    fontSizes: {
+                        sm: rem(16),
+                    },
+                    colorScheme: 'dark',
+                    colors: {
+                    // override dark colors here to change them for all components
+                    dark: [
+                    '#d5d7e0',
+                    '#acaebf',
+                    '#8c8fa3',
+                    '#666980',
+                    '#4d4f66',
+                    '#292929',
+                    '#292929',
+                    '#111111',
+                    '#0c0d21',
+                    '#01010a',
+                    ],
                 },
-                colorScheme: 'dark',
-                colors: {
-                // override dark colors here to change them for all components
-                dark: [
-                '#d5d7e0',
-                '#acaebf',
-                '#8c8fa3',
-                '#666980',
-                '#4d4f66',
-                '#292929',
-                '#292929',
-                '#111111',
-                '#0c0d21',
-                '#01010a',
-                ],
-            },
-            }}>
-            <DataTable
-                withBorder={false}
-                fontSize="sm"
-                borderRadius="sm"
-                withColumnBorders
-                highlightOnHover
-                records={rows}
-                sx={{maxWidth: "50%", zIndex: 10000, maxHeight: "25%", overflow: "auto"}}
-                columns={head}
-            />
-        </MantineProvider>
+                }}>
+                <DataTable
+                    withBorder={false}
+                    fontSize="sm"
+                    borderRadius="sm"
+                    withColumnBorders
+                    highlightOnHover
+                    records={rows}
+                    sx={{maxWidth: "50%",marginTop:5, zIndex: 10000, maxHeight: "25%", overflow: "auto"}}
+                    columns={head}
+                />
+            </MantineProvider>
+        </div>
     );
 }
 
