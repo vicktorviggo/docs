17fb4f05b7d3e89cd16a7c3e64b1ee9acac9971d
Mycroft <kaseyfitton@hotmail.co.uk>
2023-09-23 16:07:20 +0100

    move the provider to the page

--- components/table.tsx
+++ components/table.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { DataTable } from 'mantine-datatable';
-import { MantineProvider, rem } from '@mantine/core';
 
 interface DialogTabProps {
     head: any,
@@ -17,27 +16,6 @@ const Table: React.FC<DialogTabProps> = ({
 }) => {
     return (
         <div style={{ zIndex: 10000 }}>
-            <MantineProvider theme={{
-                    fontSizes: {
-                        sm: rem(16),
-                    },
-                    colorScheme: 'dark',
-                    colors: {
-                    // override dark colors here to change them for all components
-                    dark: [
-                    '#d5d7e0',
-                    '#acaebf',
-                    '#8c8fa3',
-                    '#666980',
-                    '#4d4f66',
-                    '#292929',
-                    '#292929',
-                    '#111111',
-                    '#0c0d21',
-                    '#01010a',
-                    ],
-                },
-                }}>
                 <DataTable
                     withBorder={false}
                     fontSize="sm"
@@ -48,7 +26,6 @@ const Table: React.FC<DialogTabProps> = ({
                     sx={{maxWidth: "50%",marginTop:5, zIndex: 10000, maxHeight: "25%", overflow: "auto"}}
                     columns={head}
                 />
-            </MantineProvider>
         </div>
     );
 }
